f55858260daa9e296660ba0937ed66cb914d4722
Conan Bitter <conan.bitter@gmail.com>
2022-04-26 12:43:05 +0300

    Add particle test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 #include "gfx/gfx.hpp"
+#include <random>
+
+struct Particle {
+    double x;
+    double y;
+    double dx;
+    double dy;
+    Gfx::Color color;
+};
 
 class App : public Gfx::GfxApp {
    private:
@@ -7,19 +16,44 @@ class App : public Gfx::GfxApp {
     int doty = 10;
     int sx = 1;
     int sy = 1;
+    std::vector<Particle> particles;
 
    public:
     App() {
         setTitle("YASR");
         setSize(320, 200, 3);
+        std::uniform_real_distribution<double> speed(-1.5, 1.5);
+        std::uniform_real_distribution<double> width(1.0, 319.0);
+        std::uniform_real_distribution<double> height(1.0, 199.0);
+        std::uniform_int_distribution<int> color(50, 255);
+        std::mt19937 gen;
+
+        for (int i = 0; i < 5000; i++) {
+            particles.push_back(Particle{
+                width(gen),
+                height(gen),
+                speed(gen),
+                speed(gen),
+                Gfx::Color(color(gen), color(gen), color(gen), 255)});
+        }
     }
     void onUpdate() override {
-        dotx += sx;
+        /*dotx += sx;
         doty += sy;
         if (dotx >= 319 || dotx <= 0) sx = -sx;
-        if (doty >= 199 || doty <= 0) sy = -sy;
+        if (doty >= 199 || doty <= 0) sy = -sy;*/
         renderer.clear(0);
-        renderer.putPixel(dotx, doty, 0xFF00FFFF);
+        for (Particle& p : particles) {
+            if ((p.x + p.dx) > 320.0 || (p.x + p.dx) < 0.0) {
+                p.dx = -p.dx;
+            }
+            if ((p.y + p.dy) > 200.0 || (p.y + p.dy) < 0.0) {
+                p.dy = -p.dy;
+            }
+            p.x += p.dx;
+            p.y += p.dy;
+            renderer.putPixel(p.x, p.y, p.color);
+        }
     }
 };
 
